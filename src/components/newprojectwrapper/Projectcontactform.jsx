7cbdba5127586e090940eddb53a78742d39d75ca
Mahesh0237 <upparimahesh143@gmail.com>
2025-05-05 23:27:12 +0530

    newupdates

--- src/components/newprojectwrapper/Projectcontactform.jsx
+++ src/components/newprojectwrapper/Projectcontactform.jsx
@@ -1,4 +1,4 @@
-import { BackgroundImage, Button, Card, Center, Checkbox, Container, Divider, Grid, Group, LoadingOverlay, Select, Stack, Text, Textarea, TextInput } from '@mantine/core'
+import { BackgroundImage, Button, Card, Center, Checkbox, Container, Divider, Grid, Group, LoadingOverlay, NumberInput, Select, Stack, Text, Textarea, TextInput } from '@mantine/core'
 import React, { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 import { notifications } from '@mantine/notifications'
@@ -19,9 +19,27 @@ function Projectcontactform({ pdf, project_name }) {
     }
     const [phoneNumber, setPhoneNumber] = useState('')
     const [phoneNumberError, setPhoneNumberError] = useState('')
-    const updatePhonNumber = (e) => {
-        setPhoneNumber(e.currentTarget.value)
-        setPhoneNumberError('')
+    const updatePhonNumber = (value) => {
+        setPhoneNumber(value)
+        if (value.length > 10) {
+            setPhoneNumberError("please enter a valid phone number")
+        } else {
+            setPhoneNumberError('')
+        }
+    }
+
+    const [project, setProject] = useState(null)
+    const [projectError, setProjectError] = useState('')
+    const updateProject = (value) => {
+        setProject(value)
+        setProjectError('')
+    }
+
+    const [message, setMessage] = useState('')
+    const [messageError, setMessageError] = useState('')
+    const updateMessage = (e) => {
+        setMessage(e.currentTarget.value)
+        setMessageError('')
     }
 
     const validateEmail = (email) => {
@@ -55,15 +73,25 @@ function Projectcontactform({ pdf, project_name }) {
             setPhoneNumberError('enter phone number')
             setIsLoadingEffect(false)
             return false
-        } else if (!validatePhoneNumber(phoneNumber)) {
+        } else if (!validatePhoneNumber(phoneNumber) || phoneNumber.length > 10) {
             setPhoneNumberError('Enter a valid 10-digit phone number')
+            setIsLoadingEffect(false)
+            return false
+        }
+        if (project === null) {
+            setProjectError('Select project')
+            setIsLoadingEffect(false)
+            return false
+        }
+        if (message === '') {
+            setMessageError('Please enter message')
             setIsLoadingEffect(false)
             return false
         }
 
         emailjs
-            .sendForm('service_4nqr9zl', 'template_m9b92vo', form.current, {
-                publicKey: 'Or1S7AvX78z2ZfC7-',
+            .sendForm('service_d7wvtei', 'template_k4xn7ns', form.current, {
+                publicKey: 'bToSMvPR5w8Bzed1G',
             })
             .then(
                 () => {
@@ -78,7 +106,8 @@ function Projectcontactform({ pdf, project_name }) {
                     setName('')
                     setEmail('')
                     setPhoneNumber('')
-
+                    setProject(null)
+                    setMessage('')
                 },
                 (error) => {
                     setIsLoadingEffect(false)
@@ -100,7 +129,7 @@ function Projectcontactform({ pdf, project_name }) {
                         <Text ta="center" fw={700} size='lg'>Pre-register here for best offers</Text>
                     </Card.Section>
                     <Card.Section inheritPadding withBorder py={10}>
-                        <Stack>
+                        <Stack gap={3}>
                             <TextInput
                                 placeholder='Enter Name'
                                 label="Name"
@@ -121,27 +150,54 @@ function Projectcontactform({ pdf, project_name }) {
                                 onChange={updateEmail}
                                 autoComplete='off'
                             />
-                            <TextInput
+                            <NumberInput
+                                hideControls
                                 placeholder='Enter phone number'
                                 label="Phone Number"
                                 withAsterisk
                                 name='phoneNumber'
-                                type='number'
                                 value={phoneNumber}
                                 error={phoneNumberError}
                                 onChange={updatePhonNumber}
                                 autoComplete='off'
                             />
-                            <input
-                                type="hidden"
-                                name="project_name"
-                                value={project_name}
+                            <Select
+                                label="Project"
+                                placeholder="Select Project"
+                                searchable
+                                name='project'
+                                data={[
+                                    { value: 'Sathvik Fortune City', label: 'Sathvik Fortune City' },
+                                    { value: 'Sathvik Amogha City', label: 'Sathvik Amogha City' },
+                                    { value: 'Sathvik Fusion City', label: 'Sathvik Fusion City' },
+                                    { value: 'Sathvik Dream City', label: 'Sathvik Dream City' },
+                                    { value: 'Sathvik Avenues', label: 'Sathvik Avenues' },
+                                    { value: 'Prerana Greens', label: 'Prerana Greens' },
+                                    { value: 'Greenwood Meadows', label: 'Greenwood Meadows' },
+                                    { value: 'Green Avenues', label: 'Green Avenues' },
+                                    { value: 'Sathvik Elite', label: 'Sathvik Elite' },
+                                    { value: 'Sathvik Lotus Farm', label: 'Sathvik Lotus Farm' },
+                                    { value: 'Sathvik Snow City', label: 'Sathvik Snow City' },
+                                    { value: 'Sathvik Golden City', label: 'Sathvik Golden City' },
+                                ]}
+                                value={project}
+                                onChange={updateProject}
+                                error={projectError}
                             />
-                            <Checkbox label="Agree to our Disclaimer and Terms and Conditions." size='xs' />
-                            <Button fullWidth size='md' color='#0b9444' onClick={handleSubmit}>Pre Register Here</Button>
+                            <Textarea
+                                label="Message"
+                                name='message'
+                                placeholder="Enter Message"
+                                value={message}
+                                onChange={updateMessage}
+                                error={messageError}
+                            />
+                            <Checkbox label="Agree to our Disclaimer and Terms and Conditions." size='xs' checked color='#0b9444' />
+                            <Button fullWidth size='md' color='#0b9444' type='submit'>Pre Register Here</Button>
                             <Divider label="OR" labelPosition="center" size="md" />
-                            <Text fw={500} size='lg' ta="center">Call Us: +91-9071288899</Text>
-                            <Text ta="center">For more information</Text>
+                            <Group py={10}>
+                                <Text fw={500} size='lg'>For more information Call Us: +91-9071288899</Text>
+                            </Group>
                             <Button leftSection={<IconDownload size={18} />} size='md' variant='outline' color='#0b9444' component='a' href={pdf} download="Brochure">Download Brochure</Button>
                         </Stack>
                     </Card.Section>
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,13 @@ import './App.css'
 import '@mantine/carousel/styles.css';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import '@mantine/notifications/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MantineProvider>
+      <Notifications  position='top-right'/>
       <App />
     </MantineProvider>
   </React.StrictMode>,
